3329fd2d4b7a32622df83283212c6a1cda693f88
Christian Sheridan <christiansheridan@outlook.com>
2023-01-13 08:46:23 -0600

    Add mapValidationJson function

--- src/Falco.RequestValidation/Library.fs
+++ src/Falco.RequestValidation/Library.fs
@@ -1,5 +1,17 @@
 ﻿namespace Falco.RequestValidation
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open Falco
+
+[<RequireQualifiedAccess>]
+module Request =
+    let mapValidateJson
+        (validator: 'a -> Result<'b, Map<string, string list>>)
+        (onSuccess: 'b -> HttpHandler)
+        (onValidationErrors: Map<string, string list> -> HttpHandler)
+        =
+        let handleOk (record: 'a) : HttpHandler =
+            match validator record with
+            | Ok result -> onSuccess result
+            | Error validationErrors -> onValidationErrors validationErrors
+
+        Request.mapJson handleOk
