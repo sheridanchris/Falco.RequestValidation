47b17a81683fa6bd4495b3d93e506fc45d69a562
Christian Sheridan <christiansheridan@outlook.com>
2023-01-14 21:51:45 -0600

    Add `mapValidateRoute` and refactor

--- src/Falco.RequestValidation/Library.fs
+++ src/Falco.RequestValidation/Library.fs
@@ -4,14 +4,27 @@ open Falco
 
 [<RequireQualifiedAccess>]
 module Request =
+    let private validate
+        (validator: 'value -> Result<'success, 'validationErrors>)
+        (onSuccess: 'success -> HttpHandler)
+        (onValidationErrors: 'validationErrors -> HttpHandler)
+        (value: 'value)
+        =
+        match validator value with
+        | Ok result -> onSuccess result
+        | Error validationErrors -> onValidationErrors validationErrors
+
     let mapValidateJson
         (validator: 'record -> Result<'success, 'validationErrors>)
         (onSuccess: 'success -> HttpHandler)
         (onValidationErrors: 'validationErrors -> HttpHandler)
         =
-        let handleOk (record: 'record) : HttpHandler =
-            match validator record with
-            | Ok result -> onSuccess result
-            | Error validationErrors -> onValidationErrors validationErrors
+        Request.mapJson (validate validator onSuccess onValidationErrors)
 
-        Request.mapJson handleOk
+    let mapValidateRoute
+        (reader: RouteCollectionReader -> 'value)
+        (validator: 'value -> Result<'success, 'validationErrors>)
+        (onSuccess: 'success -> HttpHandler)
+        (onValidationErrors: 'validationErrors -> HttpHandler)
+        =
+        Request.mapRoute reader (validate validator onSuccess onValidationErrors)
